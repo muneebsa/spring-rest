a04f51c1bbf5e45c01d03b841996e06fd103416b
muneebsa <muneebsa@yahoo.com>
2019-08-10 18:41:35 -0500

    Update GreetingController.java

--- src/main/java/com/examples/GreetingController.java
+++ src/main/java/com/examples/GreetingController.java
@@ -12,4 +12,10 @@ public class GreetingController {
 		Greeting response = new Greeting(name);	
 		return response;
 	}
+	
+	@RequestMapping("/greeting2")
+	public Greeting greeting2(@RequestParam(defaultValue="World") String name) {
+		Greeting response = new Greeting(name);	
+		return response;
+	}
 }
